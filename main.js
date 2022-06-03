fa75caeb56210ebf11cb59f11a31bc96480142b8
chiraagv-ltm <82768667+chiraagv-ltm@users.noreply.github.com>
2022-06-03 19:40:14 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+video="";
+status="";
+
+function preload(){
+
+
+}
+
+function setup(){
+canvas=createCanvas(500,400);
+canvas.center();
+
+video=createCapture(VIDEO);
+video.hide();
+
+}
+
+function draw(){
+    image(video,0,0,500,400);
+}
+
+function start(){
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+
+}
+
+function modelLoaded(){
+    console.log("model loaded");
+    status="true";
+
+
+}
